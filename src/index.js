87f4f6f0b89396bcffa12c2e2a8160c82cc187f1
Remulo Costa <remulo.costa@vulcanet.com.br>
2023-04-03 00:14:04 -0300

    :bug: Add create customer account endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 // import libraries
 const express = require('express');
+const {v4: uuidv4 } = require('uuid'); 
 
 // init app instance
 const app = express();
+app.use(express.json())
+
+// customers list
+/**
+ * cpf: string
+ * name: string
+ * id: string
+ * statement: []
+ */
+const customers = [];
+
+// account create route
+app.post('/account', (req, res) => {
+    // get cpf, name from request
+    const { cpf, name} = req.body;
+    // generate id
+    const id = uuidv4();
+
+    // store customer
+    customers.push({
+        id,
+        cpf,
+        name,
+        statement: []
+    });
+
+    // return created state with customer id
+    return res.status(201).send({id: id});
+})
 
 // init listeners app port
 app.listen(3333);
